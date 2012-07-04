91cc7d23a78071bc02b34576c2dfef0a73d61e07
phan <phan@temp-K43E>
2012-07-04 10:41:16 +0700

    Initialize phai (library)

---
+++ phai/Phai.cpp
@@ -0,0 +1,21 @@
+/*
+ * Phai.cpp
+ *
+ *  Created on: Jul 4, 2012
+ *      Author: phan
+ */
+
+#include "Phai.hpp"
+
+namespace phai {
+
+Phai::Phai() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Phai::~Phai() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace phai */
---
+++ phai/Phai.hpp
@@ -0,0 +1,20 @@
+/*
+ * Phai.hpp
+ *
+ *  Created on: Jul 4, 2012
+ *      Author: phan
+ */
+
+#ifndef PHAI_HPP_
+#define PHAI_HPP_
+
+namespace phai {
+
+class Phai {
+public:
+	Phai();
+	virtual ~Phai();
+};
+
+} /* namespace phai */
+#endif /* PHAI_HPP_ */
